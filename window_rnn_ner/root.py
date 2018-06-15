91baf87670cf8f8728e6735d05a63ce7eb868f75
Yeongsu Kim <kys5670i@naver.com>
2018-06-15 21:52:31 +0900

    Update root.py

--- window_rnn_ner/root.py
+++ window_rnn_ner/root.py
@@ -2,8 +2,8 @@
 import sys
 import os
 
-total_epoch = int(sys.argv[1])
-for i in range(total_epoch):
+total_num_exe = int(sys.argv[1])
+for i in range(total_num_exe):
     os.system("python q2_NER.py")
 
 
